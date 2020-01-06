b93c687594e213cf306ab4823819017ba15a2493
Vegdahl, Steve <vegdahl@up.edu>
2020-01-06 08:09:22 -0800

    Initial commit.

---
+++ app/src/main/java/edu/up/cs301/pig/PigComputerPlayer.java
@@ -0,0 +1,34 @@
+package edu.up.cs301.pig;
+
+import edu.up.cs301.game.GameFramework.GameComputerPlayer;
+import edu.up.cs301.game.GameFramework.actionMessage.GameAction;
+import edu.up.cs301.game.GameFramework.infoMessage.GameInfo;
+import edu.up.cs301.game.GameFramework.utilities.Tickable;
+
+/**
+ * An AI for Pig
+ *
+ * @author Andrew M. Nuxoll
+ * @version August 2015
+ */
+public class PigComputerPlayer extends GameComputerPlayer {
+
+    /**
+     * ctor does nothing extra
+     */
+    public PigComputerPlayer(String name) {
+        super(name);
+    }
+
+    /**
+     * callback method--game's state has changed
+     *
+     * @param info
+     * 		the information (presumably containing the game's state)
+     */
+    @Override
+    protected void receiveInfo(GameInfo info) {
+        // TODO  You will implement this method
+    }//receiveInfo
+
+}
